a67f462f7a2906cded4dae25db6a5fb0ab8a3e1b
Michael <mrsyrett@gmail.com>
2016-09-07 04:03:27 +0000

    160906.3 completed calculator functionality

--- CalculatorApp/CalcApp.js
+++ CalculatorApp/CalcApp.js
@@ -1,35 +1,108 @@
 $(document).ready(function(){
        var ray1 = "";
+       var re = /[+-/*]/g;
        $("#one").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(1);
-              
        })
        $("#two").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(2);
        })
         $("#three").on('click', function(){
               $(".window").append(3);
        })
         $("#four").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(4);
        })
         $("#five").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(5);
        })
         $("#six").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(6);
        })
         $("#seven").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(7);
        })
         $("#eight").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(8);
        })
         $("#nine").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(9);
        })
         $("#zero").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
               $(".window").append(0);
+       })
+         $("#dot").on('click', function(){
+        if ($("#add").hasClass("clicked") || $("#sub").hasClass("clicked") || $("#divi").hasClass("clicked") || $("#mult").hasClass("clicked")){
+              $(".window").text('');
+              $("#add").removeClass("clicked");
+              $("#sub").removeClass("clicked");
+              $("#divi").removeClass("clicked");
+              $("#mult").removeClass("clicked");
+        }
+              $(".window").append('.');
        })
        $("#clear").on('click', function(){
               $('.window').text('');
@@ -40,19 +113,19 @@ $(document).ready(function(){
        })
        $("#add").on('click', function(){
               ray1 = $('.window').text() + " + ";
-              $(".window").text('');
+              $("#add").addClass("clicked");
        })
        $("#sub").on('click', function(){
               ray1 = $('.window').text() + " - ";
-              $(".window").text('');
+              $("#sub").addClass("clicked");
        })
        $("#divi").on('click', function(){
               ray1 = $('.window').text() + " / ";
-              $(".window").text('');
+              $("#divi").addClass("clicked");
        })
        $("#mult").on('click', function(){
               ray1 = $('.window').text() + " * ";
-              $(".window").text('');
+              $("#mult").addClass("clicked");
        })
        $("#perc").on('click', function(){
               var perc = parseInt($('.window').text()) / 100;
@@ -61,7 +134,6 @@ $(document).ready(function(){
        })
        $("#equal").on('click', function(){
               ray1 += $('.window').text();
-              //var sum = parseInt(ray1[0]) + parseInt(ray2[0]);
               var result = eval(ray1);
               $(".window").text(result);
        })
